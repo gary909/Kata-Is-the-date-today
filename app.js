acbac1b2eb1842cb03550acb2c8c74e954b88cf1
gary <gary.white@lodgit.com>
2022-03-08 13:18:08 +0100

    code working

---
+++ app.js
@@ -0,0 +1,18 @@
+function isToday(date) {
+    var myDate = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    myDate = mm + '/' + dd + '/' + yyyy;
+
+    if (myDate == 'today') {
+        return true;
+    } else {
+        return false
+    }
+}
+
+
+console.log(isToday(12)); // 
+console.log(isToday(12)); // 
